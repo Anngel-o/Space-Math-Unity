b86e551da789add78356d18b4769db616e71590b
Ángel Ortiz <anngelo.omz@gmail.com>
2024-04-17 22:18:56 -0600

    Android SDK configure & player touch movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 10f;
+    public float velocity = 10f;
+    public float controlX, controlY;
     Rigidbody2D rbPlayer;
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // float movement = Input.GetAxisRaw("Horizontal");
+        // rbPlayer.velocity = new Vector2(movement * velocity, rbPlayer.velocity.y);
 
+        // Comprueba si hay un toque en la pantalla
+        if (Input.touchCount > 0)
+        {
+            // Obtén el primer toque en la pantalla
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    controlX = touchPosition.x - transform.position.x;
+                    controlY = touchPosition.y - transform.position.y;
+                    break;
+                
+                case TouchPhase.Moved:
+                    rbPlayer.MovePosition(new Vector2(touchPosition.x - controlX, touchPosition.y - controlY));
+                    break;
+
+                case TouchPhase.Ended:
+                    rbPlayer.velocity = Vector2.zero;
+                    break;
+            }
+        }
     }
 }
